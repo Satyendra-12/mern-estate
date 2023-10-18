60a68fd202f7a64230c5592665d25737bc9d0495
Satyendra Kumar <satyendrakumar8672461@gmail.com>
2023-10-18 13:23:31 +0530

    header

--- src/app.jsx
+++ src/app.jsx
@@ -5,10 +5,12 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/profile';
+import Header from './components/Header';
 export function App() {
  return (
  <div>
   <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/sign-in' element={<SignIn/>}/>
